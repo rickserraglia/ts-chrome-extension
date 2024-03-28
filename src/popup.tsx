583b5d677d1897334c742e50fc0a43d4109ba1ac
Henrique Serraglia <ricks@duck.com>
2024-03-28 10:26:10 -0300

    Adds shadcn-ui (w/ tabs component), removes components that won't be used and makes minor changes to tsconfig and prettierrc

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,11 +1,45 @@
-import { CountButton } from '~features/count-button';
+import '@/styles/globals.css';
 
-import '~style.css';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { NewSchedule } from '@/tabs/new-schedule';
+import { Schedules } from '@/tabs/schedules';
+import { Settings } from '@/tabs/settings';
+
+interface Tab {
+	value: string;
+	label: string;
+	Content: () => JSX.Element;
+}
+
+const tabs: Tab[] = [
+	{ value: 'schedules', label: 'Schedules', Content: () => <Schedules /> },
+	{
+		value: 'new-schedule',
+		label: 'New Schedule',
+		Content: () => <NewSchedule />,
+	},
+	{ value: 'settings', label: 'Settings', Content: () => <Settings /> },
+];
 
 function IndexPopup() {
 	return (
-		<div className="flex items-center justify-center h-16 w-40">
-			<CountButton />
+		<div className="flex flex-1 justify-center max-h-screen">
+			<Tabs defaultValue={tabs[0].value}>
+				<TabsList>
+					{tabs.map((tab) => (
+						<TabsTrigger key={tab.value} value={tab.value}>
+							{tab.label}
+						</TabsTrigger>
+					))}
+				</TabsList>
+				<div className="flex-1 flex items-center justify-center">
+					{tabs.map(({ value, Content }) => (
+						<TabsContent key={value} value={value}>
+							<Content key={value} />
+						</TabsContent>
+					))}
+				</div>
+			</Tabs>
 		</div>
 	);
 }
---
+++ src/tabs/new-schedule.tsx
@@ -0,0 +1,3 @@
+export const NewSchedule = () => {
+	return <div>New Schedule</div>;
+};
